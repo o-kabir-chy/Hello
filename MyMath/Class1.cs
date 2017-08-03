8b35cac3e8654400a26ff92006357785f5acb956
Obaidul Kabir <o_kabir_chy@yahoo.com>
2017-08-03 21:36:14 +0600

    first commit

---
+++ MyMath/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyMath
+{
+    public class Math
+    {
+        public int add(int a,int b) { return a + b; }
+    }
+}
